1cd2573a24b1e4ec4706b2d66c5aab14e146715e
Adi Adnan <adi.adnan15@yahoo.com>
2016-06-14 00:09:44 +0300

    Need to finish login+register

---
+++ public/javascripts/authentication.js
@@ -0,0 +1,30 @@
+jQuery(function($){
+	var login_fields = $('#login input');
+	var register_fields = $('#register_input');
+
+	$('#login form').submit(login);
+	$('#register form').submit(register);
+
+	function login(e){
+		e.preventDefault();
+		var relevant_data = {
+			'user' : login_fields[0].value,
+			'pass' : login_fields[0].value
+		};
+		$.ajax({
+			type : 'POST',
+			url : '/authentication/login',
+			data : relevant_data
+		}).done(function(response){
+			console.log(response);
+			location.reload();
+		}).fail(function(a,b,c){
+			if(a.status == 500){
+				alert(a.responseText);
+			}
+			if(a.status == 401){
+				alert(a.responseText);
+			}
+		});
+	}
+})
